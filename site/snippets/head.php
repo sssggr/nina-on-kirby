388eb4faf27f75a5d936b39544b207154726702f
Nina Siessegger <n.siessegger@zeromail.org>
2024-09-17 11:12:18 +0200

    Revert "Add tracking snippet"
    This reverts commit b82657780c73894a3dab42318e0351a698bee12e.

--- site/snippets/head.php
+++ site/snippets/head.php
@@ -19,7 +19,4 @@
     'assets/css/vendor/flexboxgrid.min.css',
     'assets/css/base.css',
     '@auto']) ?>
-    <!-- 100% privacy-first analytics -->
-    <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
-    <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt="" referrerpolicy="no-referrer-when-downgrade" /></noscript>
 </head>
