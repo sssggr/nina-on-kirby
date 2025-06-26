635a3515217d3d88eac653a6ee5e6e8a123bdd79
Nina Siessegger <n.siessegger@zeromail.org>
2025-06-26 12:08:47 +0200

    Use grid

--- site/snippets/blocks/portrait-text.php
+++ site/snippets/blocks/portrait-text.php
@@ -2,13 +2,17 @@
 $portrait = $block->portrait()->toFiles()->first();
 $text = $block->text();
 ?>
-<section class="portrait-text-block">
-  <?php if ($portrait): ?>
-    <div class="portrait-image-container">
-      <img src="<?= $portrait->url() ?>" alt="<?= $portrait->alt()->or('Portrait') ?>" class="portrait-image" loading="lazy" />
-    </div>
-  <?php endif; ?>
-  <div class="portrait-text-content">
-    <?= kirbytext($text) ?>
+<div class="row">
+  <div class="col-xs-12 col-md-offset-1 col-lg-offset-2 col-lg-9 col-md-10">
+    <section class="portrait-text-block">
+      <?php if ($portrait): ?>
+        <div class="portrait-image-container">
+          <img src="<?= $portrait->url() ?>" alt="<?= $portrait->alt()->or('Portrait') ?>" class="portrait-image" loading="lazy" />
+        </div>
+      <?php endif; ?>
+      <div class="portrait-text-content">
+        <?= kirbytext($text) ?>
+      </div>
+    </section>
   </div>
-</section> 
+</div> 
