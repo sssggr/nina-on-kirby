57d3a1f4b40f2b95cd19fb38bf8e5c6447727294
Nina Siessegger <n.siessegger@zeromail.org>
2025-09-15 15:20:48 +0200

    Update offer packages design

--- site/snippets/blocks/offer-packages.php
+++ site/snippets/blocks/offer-packages.php
@@ -1,51 +1,74 @@
 <?php
 $offers = $block->offers()->toStructure();
-$resultLabel = t('offer.result', 'Ergebnis');
-$defaultButton = t('offer.button', 'Interessiert? Jetzt anfragen');
+$defaultButton = t('offer.button');
 ?>
 <section class="packages">
-  <div class="row">
-    <?php foreach ($offers as $offer): ?>
-      <div class="col-xs-12 col-md-4">
-        <div class="package-card">
-          <div class="package-card-content">
-            <h3><?= $offer->title()->html() ?></h3>
-            <div class="package-card-target">
-              <p class="target"><?= $offer->target()->html() ?></p>
-            </div>
-            <div class="package-card-features">
-              <?php
-                $featuresRaw = $offer->features()->value();
-                $features = $offer->features()->yaml();
-                if (is_array($features) && count($features) > 1) {
-                  $featuresList = $features;
-                } else {
-                  preg_match_all('/<li>(.*?)<\/li>/is', $featuresRaw, $matches);
-                  $featuresList = $matches[1] ?? [];
-                }
-              ?>
-              <ul>
-                <?php foreach ($featuresList as $feature): ?>
-                  <li><?= str_replace('&amp;', '&', esc(strip_tags($feature))) ?></li>
-                <?php endforeach ?>
-              </ul>
-            </div>
-          </div>
-          <?php if ($offer->result()->isNotEmpty()): ?>
-            <p class="result"><?= $resultLabel ?>: <?= $offer->result()->html() ?></p>
-          <?php endif ?>
-          <?php if ($offer->price()->isNotEmpty()): ?>
-            <p class="price"><?= $offer->price()->html() ?></p>
-          <?php endif ?>
-          <?php
-            $buttonPage = $offer->buttonurl()->toPage();
-            $buttonHref = $buttonPage ? $buttonPage->url() : null;
-          ?>
-          <?php if ($buttonHref): ?>
-            <a href="<?= $buttonHref ?>" class="btn"><?= $offer->buttontext()->or($defaultButton)->html() ?></a>
-          <?php endif ?>
+  <?php foreach ($offers as $offer): ?>
+    <div class="package-card">
+      <div class="row">
+        <!-- Title: volle Breite -->
+        <div class="col-xs-12">
+          <h3 class="package-title"><?= $offer->title()->html() ?></h3>
         </div>
       </div>
-    <?php endforeach ?>
-  </div>
+      
+      <div class="row">
+        <div class="col-xs-12 col-md-6">
+          <div class="package-section target-section">
+            <h4><?= t('offer.whom') ?> <span class="target"><?= $offer->target()->html() ?></span></h4>
+          </div>
+        </div>
+        
+        <div class="col-xs-12 col-md-6">
+          <div class="package-section features-section">
+            <?php
+              $featuresRaw = $offer->features()->value();
+              $features = $offer->features()->yaml();
+              if (is_array($features) && count($features) > 1) {
+                $featuresList = $features; 
+              } else {
+                preg_match_all('/<li>(.*?)<\/li>/is', $featuresRaw, $matches);
+                $featuresList = $matches[1] ?? [];
+              }
+            ?>
+            <ul>
+              <?php foreach ($featuresList as $feature): ?>
+                <li><?= str_replace('&amp;', '&', esc(strip_tags($feature))) ?></li>
+              <?php endforeach ?>
+            </ul>
+          </div>
+        </div>
+      </div>
+      
+      <div class="row">
+        <div class="col-xs-12 <?= $offer->price()->isNotEmpty() ? 'col-md-8' : 'col-md-12' ?>">
+          <div class="package-section result-section">
+            <h4><?= t('offer.result') ?> <span class="result"><?= $offer->result()->html() ?></span></h4>
+          </div>
+        </div>
+        
+        <?php if ($offer->price()->isNotEmpty()): ?>
+          <div class="col-xs-12 col-md-4">
+            <div class="package-section price-section">
+              <p class="price"><?= $offer->price()->html() ?></p>
+            </div>
+          </div>
+        <?php endif ?>
+      </div>
+      
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="package-section button-section">
+            <?php
+              $buttonPage = $offer->buttonurl()->toPage();
+              $buttonHref = $buttonPage ? $buttonPage->url() : null;
+            ?>
+            <?php if ($buttonHref): ?>
+              <a href="<?= $buttonHref ?>" class="btn"><?= $offer->buttontext()->or($defaultButton)->html() ?></a>
+            <?php endif ?>
+          </div>
+        </div>
+      </div>
+    </div>
+  <?php endforeach ?>
 </section> 
