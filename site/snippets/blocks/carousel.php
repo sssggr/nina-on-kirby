08520d805817853b1bbc14c9a57e2c8ad78fa99f
Nina Siessegger <n.siessegger@zeromail.org>
2026-01-20 11:15:34 +0100

    Fix carousel on offers page

---
+++ site/snippets/blocks/carousel.php
@@ -0,0 +1,10 @@
+<section class="testimonials">
+    <div class="row">
+        <div class="col-xs-12 col-md-offset-1 col-lg-offset-2 col-lg-9 col-md-10">
+            <?php snippet('carousel', [
+                'block' => $block,
+                'testimonials' => $block->testimonials()->toStructure(),
+            ]); ?>
+        </div>
+    </div>
+</section>
